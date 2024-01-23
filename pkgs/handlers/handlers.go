8a03d4b92a3b3fe9e992e5aa2bc615130a747cfc
thehuy212 <huyngptit@gmail.com>
2024-01-23 17:35:51 +0700

    push code

---
+++ pkgs/handlers/handlers.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"Project/pkgs/render"
+	"net/http"
+)
+
+//Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request){
+	render.RenderTemplate(w, "home.page.tmpl")
+}
+
+//About is the about page handler
+func About(w http.ResponseWriter, r *http.Request){
+	render.RenderTemplate(w, "about.page.tmpl")
+}
